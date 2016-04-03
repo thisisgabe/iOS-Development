32747866d1ba037164ed18bb083e6ea1302cd390
Gabriel Velasquez <gvelasquez@gmail.com>
2016-04-03 00:03:26 -0700

    Finished CH2
    Beginning iphone dev w/ swift
    ch2 done

--- Beginning X-Code/HelloWorld/HelloWorld/ViewController.swift
+++ Beginning X-Code/HelloWorld/HelloWorld/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblOutput: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lblOutput.text = "Bonjour!"
     }
 
     override func didReceiveMemoryWarning() {
