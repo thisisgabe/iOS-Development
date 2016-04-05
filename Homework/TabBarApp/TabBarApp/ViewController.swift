df3bbe2e7b4c14dfe442010e2e7cbaedd75b0dc5
Gabriel Velasquez <gvelasquez@gmail.com>
2016-04-05 00:32:11 -0700

    Homework 6 - Tab Bar Application
    Started the Tab Bar application with 3 tabs
    1 - convert units
    2 - info based on date entered
    3 - picker with list of books and displays pic of cover and synopsis

---
+++ Homework/TabBarApp/TabBarApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarApp
+//
+//  Created by Gabriel Velasquez on 4/4/16.
+//  Copyright © 2016 CSUSM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
